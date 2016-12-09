038b507a8d881129757869509930dd4ebb6ad2f6
jayjariwala <jay.jariwala@hotmail.com>
2016-12-08 21:59:52 -0500

    fornt end final commit

--- frontend/js/jsloader.js
+++ frontend/js/jsloader.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function(){
   console.log("print this");
+
+  $('.confirm').hide();
+  $('.code_buttons').css("height","50px");
+
+  $('.rmv').click(function(){
+    $('.confirm').show();
+    $('.code_buttons').css("height","80px");
+  })
 })
