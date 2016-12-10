2d880dcd902870ce04a2e196e0a7a8fd7d4ece55
jayjariwala <jay.jariwala@hotmail.com>
2016-12-10 17:45:48 -0500

    yahoo-finance implemented

--- controller/stockController.js
+++ controller/stockController.js
@@ -1,9 +1,20 @@
+var yahooFinance = require('yahoo-finance');
 module.exports = function(app,io)
 {
   app.get('/',function(req,res){
     res.render('index');
 
-
+    yahooFinance.historical({
+      symbol:'AAPL',
+      from:'2015-01-01',
+      to:'2016-12-01',
+      period:'m'
+    },function(err,quotes){
+      console.log(quotes);
+    })
+    yahooFinance.snapshot({symbol:'AAPL',fields:['n']},function(err,snapshot){
+      console.log(snapshot);
+    })
 
     res.end();
   });
