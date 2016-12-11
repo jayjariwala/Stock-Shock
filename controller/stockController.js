2135699620b6a7011891d2a8ea13e8e204f1cc8c
jayjariwala <jay.jariwala@hotmail.com>
2016-12-11 17:34:02 -0500

    schema search for pre-existing stock

--- controller/stockController.js
+++ controller/stockController.js
@@ -15,40 +15,49 @@ module.exports = function(app,io)
   io.on('connection',function(socket){
     socket.on('stock code',function(stockCode){
       yahooFinance.snapshot({symbol:stockCode,fields:['n']},function(err,snapshot){
-        var stockObj ={
 
-        }
         if(snapshot.name==null)
         {
           io.emit('stock code','stockerr');
         }
         else {
+          stock_data.find({stock_code : snapshot.symbol},function(err,available){
+            console.log("The Stock Availability: "+available);
+          });
+
           yahooFinance.historical({
             symbol:stockCode,
-            from:'2015-01-01',
+            from:'2016-01-01',
             to:'2016-12-01',
             period:'m'
           },function(err,stock){
+
+            var timestamp = Math.floor(Date.now() /1000);
+            var processid= process.pid;
+            var ranNum = Math.random() * (100 - 0) + 100;
+            var sid= timestamp+''+processid+''+ranNum;
+            var stock_d = [];
+
+            for(var i=0 ; i< stock.length  ; i++)
+            {
+                stock_d.push(stock[i].high);
+            }
+
+        /*    console.log("This Should be 12:"+stock_d.length);
+
             var userstock = new stock_data({
-              s_id:'abc',
-              stock_code:'xyz',
-              stock_name:'lmn',
+              s_id:sid,
+              stock_code:stock,
+              stock_name:snapshot.name,
               Stock_values: ['a','b','c']
               });
 
               userstock.save(function(err){
                 if(err) throw err;
                 console.log("information stored successfully");
-                })
+              }) */
           });
         }
-
-
-
-
-
-
-
 
 
       })
