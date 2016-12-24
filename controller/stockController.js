df9602026fd1cce2f1c52cab45f6000f4a9c6c32
MrJayPeace <MrJayPeace@gmail.com>
2016-12-24 12:57:25 -0500

    send errors on specific connected user fixed

--- controller/stockController.js
+++ controller/stockController.js
@@ -35,90 +35,18 @@ var quotedAndCommaSeparated = "'" + timeValues.join("','") + "'";
   });
   io.on('connection',function(socket){
 
+
+
     var sendfinalstock=stocksdata.getallstocks(stock_data,yahooFinance,randomColor,io);
 
-  /*  var retrivedstocks =[];
-    stock_data.find({},function(err,allstocks){
-        allstocks.forEach(function(each){
-          retrivedstocks.push(each.stock_code);
-        })
-
-    yahooFinance.historical({
-          symbols:retrivedstocks,
-          from:formatedyearback,
-          to:currentdate,
-          period:'m'
-        },function(err,stockhis){
-
-var sendfinalstock =[];
-console.log("The value of final array is"+sendfinalstock.length)
-
-
-
-          retrivedstocks.forEach(function(allstocks){
-            var stockprice=[];
-            var sobj=stockhis[allstocks];
-            sobj.forEach(function(data){
-              stockprice.push(data.close);
-            })
-
-        var color = randomColor({
-         luminosity: 'bright',
-         format: 'rgb'
-      });
-      var newcolor= color.replace('rgb','rgba');
-      var ncolor=(newcolor.replace(')','')).concat(',0.4)');
-      var jcolor=ncolor.concat(',0.1)');
-      var borderColor=ncolor.concat(',1)');
-
-
-
-        console.log(color);
-            var stockobj={
-              label: allstocks,
-              fill: true,
-              lineTension: 0.1,
-              backgroundColor: jcolor,
-              borderColor: borderColor,
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: "rgba(239,107,1,1)",
-              pointBackgroundColor: "#fff",
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "rgba(255,255,255,1)",
-              pointHoverBorderColor: borderColor,
-              pointHoverBorderWidth: 5,
-              pointRadius: 5,
-              pointHitRadius: 15,
-              data: stockprice,
-              spanGaps: true,
-            }
-
-            sendfinalstock.push(stockobj);
-            console.log("The length of the object is"+allstocks);
-
-
-          });
-
-*/
-//          console.log("THIS IS FINAL BOSS"+sendfinalstock);
-
-      //    console.log(">>>>>PRINT AN OBJECT"+sendfinalstock);
-
-      //    io.emit('user connection',sendfinalstock);
-
-//        });
-//    })
 
     socket.on('stock code',function(stockCode){
       yahooFinance.snapshot({symbol:stockCode,fields:['n']},function(err,snapshot){
-
+        var sessionid=socket.id;
+        console.log("CLIENT SESSION ID:::"+sessionid);
         if(snapshot.name==null)
         {
-          io.emit('stock code','stockerr');
+           io.sockets.connected[sessionid].emit('stock code','stockerr');
         }
         else {
           stock_data.find({stock_code : stockCode},function(err,available){
@@ -136,50 +64,17 @@ console.log("The value of final array is"+sendfinalstock.length)
                   if(err) throw err;
                   console.log("information stored successfully");
                   stocksdata.getallstocks(stock_data,yahooFinance,randomColor,io);
-                /*  stock_data.find({},function(err,allstocks){
 
-                      allstocks.forEach(function(each){
-
-                        retrivedstocks.push(each.stock_code);
-                      })
-                      console.log(retrivedstocks.toString());
-                  yahooFinance.historical({
-                        symbols:retrivedstocks,
-                        from:formatedyearback,
-                        to:currentdate,
-                        period:'m'
-                      },function(err,stockhis){
-                        console.log("what is this?"+stockhis);
-                      });
-                  }) */
                 });
             }
             else {
-                  io.emit('stock code', 'stockavail');
+
+                 io.sockets.connected[sessionid].emit('stock code', 'stockavail');
+
             }
 
           });
 
-          /*
-          yahooFinance.historical({
-            symbol:stockCode,
-            from:'2016-01-01',
-            to:'2016-12-01',
-            period:'m'
-          },function(err,stock){
-
-
-            var stock_d = [];
-
-            for(var i=0 ; i< stock.length  ; i++)
-            {
-                stock_d.push(stock[i].high);
-            }
-
-        /*    console.log("This Should be 12:"+stock_d.length);
-
-             */
-      /*    });  */
 
 
 
